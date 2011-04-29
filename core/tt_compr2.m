ff796e76fdf30376b547b92371044d84fe79cad2
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-04-29 17:44:13 +0200

    Cleared something: it seems that accidentally tt_compr2.m did not include the latest changes As was the tt_mat_compr However, I would like to recommend to use "round" whenever possible

--- core/tt_compr2.m
+++ core/tt_compr2.m
@@ -26,6 +26,17 @@ if ((nargin<3)||(isempty(max_r)))
     exists_max_r=0;
 end;
 
+% if ( nrm_full < log(1e-200) ) %Something really small
+%   n=size(tt{1},1);
+%   tt{1}=zeros(n,0);
+%   n=size(tt{d},1);
+%   tt{d}=zeros(n,0);
+%   for i=2:d-1
+%   n=size(tt{i},1);
+%   tt{i}=zeros(n,0,0);
+%   end
+%   return
+% end
 %nrmf=zeros(d,1); %Place to store the norms of QR-factors
 %first, we orthogonalize the tensor from right to left until the first mode
 mat=tt{d};
@@ -44,9 +55,13 @@ for i=(d-1):-1:2
     tt{i}=permute(tt{i},[1,3,2]);
 end
 tt{1}=tt{1}*conj(rv');
+%nrmf(1)=norm(tt{1},'fro');
+%nrm_full=sum(log(nrmf(1:d))); nrm1=nrm_full/d; nrm1=exp(nrm1); %This would
+%be the norm of each core
 %Now gradually start compression from the left, using the knowledge 
 %of norms
 mat=tt{1};
+%return;
 mat(abs(mat)<1e-300)=0;
 [u0,s0,ru]=svd(mat,0); 
 %If no scaling factors were taken out,
@@ -55,32 +70,39 @@ mat(abs(mat)<1e-300)=0;
 %Here the "True" s0 matrix is up to factor of product of nrm(2:d)
 %so for it we can filter the singular values just at 
 %eps*nrmf(1)/sqrt{d-1}
+%nrmf(1)=norm(diag(s0));  
+%nrm=norm(diag(s0));
 eps1=eps*norm(diag(s0))/sqrt(d-1); %This is the absolute accuracy to filter with
+%r0=my_chop2(diag(s0),eps1);
 r0=numel(find(s0>eps1));
-if (exists_max_r) 
-    r0 = min(r0, max_r); 
-end;
+if (exists_max_r) r0 = min(r0, max_r); end;
+%keyboard;
+%r0=rank(mat,eps1);
 u0=u0(:,1:r0);
 s0=diag(s0);
 s0=s0(1:r0); % -eps1*sign(s0);
 ru=ru(:,1:r0)*diag(s0)./nrm1; %This should be scaled properly
+%ru=ru;
 tt{1}=u0.*nrm1; %Now it is properly scaled
 for i=2:d-1
     %Convolve tt{i} over the second index
+%     fprintf('norm_tt{%d}=%g, norm_ru = %g\n', i, norm(reshape(tt{i}, size(tt{i},1)*size(tt{i},2), size(tt{i},3)), 'fro'), norm(ru,'fro'));        
     tt{i}=ten_conv(tt{i},2,conj(ru));
     ncur=size(tt{i},1);
     r2=size(tt{i},2);
     r3=size(tt{i},3);
     core=reshape(tt{i},[ncur*r2,r3]);
+    %r=rank(core,eps1);
+    %keyboard;  
     core(abs(core)<1e-300)=0;
     [u0,s0,ru]=svd(core,0);
     
     nrm=norm(diag(s0));
     eps1=eps*nrm/sqrt(d-1); %Nothing more
+    %keyboard;
+    %r=my_chop2(diag(s0),eps1);
     r=numel(find(s0>eps1));
-    if (exists_max_r) 
-        r = min(r, max_r); 
-    end;
+    if (exists_max_r) r = min(r, max_r); end;
     u0=u0(:,1:r);
     s0=(s0(1:r,1:r))./nrm1;
     ru=ru(:,1:r)*s0;
