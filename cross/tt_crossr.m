630720c84cc4e8b4181216fc7e99a96bdba6e58d
Sergei Dolgov <dc@spout.(none)>
2011-05-09 17:49:50 +0400

    In my last branch merge, I realized that I merged a head of TT2.1 to master, so some of our development files were erased. Now I pulled them from the previous commit.

---
+++ cross/tt_crossr.m
@@ -0,0 +1,105 @@
+function [tt,ind_right]=tt_crossr(arr,ind_left)
+%[TT,IND_RIGHT]=TT_CROSSR(SZ,ARR,IND_LEFT)
+%SZ is the mode size of the array, 
+%ARR is the cell array with parameters to supply for my_elem
+%IND_LEFT is the left index sequence
+%TT is the approximation computed and IND_RIGHT is 
+%the computed right index sequence
+%
+%Second technical function that tries to mimic the skeleton decomposition
+%of the TT-tensor directly using properly computed index sets
+%It works! But the problem is overparametrized, since
+%ind_left is uniquely defined by ind_list :-)
+%ind_right is perfectly arbitrary.
+%IND_LEFT is known and IND_RIGHT is computed
+%Array is given implicitly, only a subroutine that
+%computes any prescribed element is known (MY_ELEM) here
+d=arr{1};
+sz=arr{2};
+%arr1=arr(:);
+tt=cell(d,1);
+ind_right=cell(d,1); %Computed ind_right
+ind_l=ind_left{d-1};
+r1=size(ind_l,2);
+ncur=sz(1);
+mat=zeros(ncur,r1);
+for j=1:ncur
+   for s=1:r1
+      ind_f=[ind_l(:,s)',j];
+      %mat(j,s)=arr1(sub_to_ind(ind_f,sz));
+      val=my_elem(ind_f,arr); 
+      mat(j,s)=val;
+   end
+end
+[qs,rs]=qr(mat,0);
+ind=maxvol2(qs);
+ind_right{d-1}=ind;
+%mat=mat*inv(mat(ind,:));
+mat=qs*inv(qs(ind,:));
+tt{d}=mat;
+
+for k=(d-1):-1:2
+     ncur=sz(k);
+     ind_l=ind_left{k-1};
+     ind_r=ind_right{k};
+     r2=size(ind_l,2);
+     r3=size(ind_r,2);
+     core=zeros(ncur,r2,r3);
+     %fprintf('k=%d \n');
+     for i=1:ncur
+         for s2=1:r2
+             for s3=1:r3
+                 ind_f=[ind_l(:,s2)',i,ind_r(:,s3)'];
+                 val=my_elem(ind_f,arr); 
+                 core(i,s2,s3)=val;
+                 %core(i,s2,s3)=arr1(sub_to_ind(ind_f,sz));
+
+             end
+         end
+     end
+     %core is ncurxr2xr3, from the left we need 
+     %r2xncurxr3, but from the right 
+     %we need (ncurxr3)xr2 or (r3xncur)xr2
+     core=permute(core,[3,1,2]); core=reshape(core,[r3*ncur,r2]);
+     rnew=min(r2,r3*ncur);
+     [qs,rs]=qr(core,0);
+     ind=maxvol2(qs);
+     ind_old=ind_right{k};
+     ind_new=zeros(d-k+1,rnew);
+     ncur=sz(k);
+     for s=1:rnew
+        f_in=ind(s);
+        w1=tt_ind2sub([r3,ncur],f_in);
+        rs=w1(1); js=w1(2);
+        ind_new(:,s)=[js,ind_old(:,rs)'];
+     end
+     ind_right{k-1}=ind_new;
+     %core=core*inv(core(ind,:)); 
+     core=qs*inv(qs(ind,:));
+     
+     % core=reshape(core,[r2,ncur,r3]); core=permute(core,[2,1,3]);
+     % core=reshape(core,[ncur,r3,r2]); core=permute(core,[1,3,2]);
+     core=reshape(core,[r3,ncur,rnew]); core=permute(core,[2,3,1]);
+   
+    tt{k}=core;
+end
+ncur=sz(1);
+ind_r=ind_right{1};
+r=size(ind_r,2);
+mat=zeros(ncur,r);
+for i=1:ncur
+    for s=1:r
+      ind_f=[i,ind_r(:,s)'];
+      val=my_elem(ind_f,arr);
+      mat(i,s)=val;
+      %mat(i,s)=arr1(sub_to_ind(ind_f,sz));
+    end
+end
+tt{1}=mat;
+%for k=1:d
+%     res=tt{k}-tt_true{k};
+%    res=res(:);
+%    fprintf('k=%d, approximation error: %e \n',k,norm(res));
+%end
+return
+end
---
+++ solve/als_prec.m
@@ -0,0 +1,81 @@
+function [x,y]=als_prec(mat,rhs,y,niter)
+%[Y]=ALS_PREC(MAT,RHS,X)
+%Computes (approximate) rank-1 solution to the
+%problems MAT*P = RHS, where A is a low-Kronecker rank matrix
+%P=X x Y, RHS is a low-Kronecker rank matrix
+%B is given as a two-2d cell array, the same is for X
+
+a=mat{1}; %The first cell
+b=mat{2}; 
+R=size(a,3); %The rank
+n=size(a,1);
+m=size(b,1);
+a1=permute(a,[1,3,2]); a1=reshape(a1,[n*R,n]);
+b1=permute(b,[1,3,2]); b1=reshape(b1,[m*R,m]);
+rhs1=rhs;
+if ( nargin < 5 )
+   niter=10;
+end
+u=rhs{1}; 
+v=rhs{2}; 
+rr=size(u,3);
+u1=reshape(u,[n*n,rr]);
+v1=reshape(v,[m*m,rr]);
+
+for i=1:niter
+    %fprintf('i=%d \n',i);
+    %at=a1*x;
+    %bt=b1*y;   
+    %Iterate over x
+    bt=b1*y;
+    %bt is n*R*n, scalar product of 
+    bt1=reshape(bt,[m,R,m]); bt1=permute(bt1,[1,3,2]); bt1=reshape(bt1,[m*m,R]);
+    p=bt1'*bt1; %p matrix is ready
+    %Compute the local matrix sum_{a,b} p_{ab} A^{\top}_b A_a,
+    %it is A(k,i,b)*A(k,j,a)*p(a,b)
+    a2=reshape(a,[n*n,R]); a2=a2*p; %a2 is (k,j,b), sum over (k,b) with (k,i,b)
+    a3=reshape(a,[n,n,R]); a3=permute(a3,[2,1,3]); a3=reshape(a3,[n,n*R]);
+    a2=reshape(a2,[n,n,R]); a2=permute(a2,[2,1,3]); a2=reshape(a2,[n,n*R]);
+    loc_mat=a3*a2'; %Should be valid even for complex numbers
+    %For the right hand side
+    %Compute the q matrix <b,v> = b is m x m x R, V is m x m x rr
+    %the result is R x rr
+    q=bt1'*v1; %q is R x rr
+    %Now compute right-hand side as
+    %q(R,rr)*U(i,k,rr)*A(j,k,R)
+    
+    u2=u1*q'; % u2 is (i,k,R)
+    u2=reshape(u2,[n,n*R]);
+    a2=reshape(a,[n,n*R]);
+    rhs=u2*a2';
+    x= loc_mat \ rhs;
+    %cond(loc_mat)
+    %Iterate over y
+    at=a1*x;
+    %bt is n*R*n, scalar product of 
+    at1=reshape(at,[n,R,n]); at1=permute(at1,[1,3,2]); at1=reshape(at1,[n*n,R]);
+    p=at1'*at1; %p matrix is ready
+    %Compute the local matrix sum_{a,b} p_{ab} A^{\top}_b A_a,
+    %or the summation: \sum_{a,b,k} p(a,b) A(i,k,a)*A(j,k,b)
+    b2=reshape(b,[m*m,R]); b2=b2*p; %a2 is (k,j,b), sum over (k,b) with (k,i,b)
+    b3=reshape(b,[m,m,R]); b3=permute(b3,[2,1,3]); b3=reshape(b3,[m,m*R]);
+    b2=reshape(b2,[m,m,R]); b2=permute(b2,[2,1,3]); b2=reshape(b2,[m,m*R]);
+    loc_mat=b3*b2'; %Should be valid even for complex numbers
+    %For the right hand side
+    %Compute the q matrix <b,v> = b is m x m x R, V is m x m x rr
+    %the result is R x rr
+    q=at1'*u1; %q is R x rr
+    %Now compute right-hand side as
+    %q(R,rr)*U(i,k,rr)*A(j,k,R)
+    
+    v2=v1*q'; % u2 is (i,k,R)
+    v2=reshape(v2,[m,m*R]);
+    b2=reshape(b,[m,m*R]);
+    rhs=v2*b2';
+    y= loc_mat \ rhs;
+    %    norm(tt_matrix(mat)*kron(tt_matrix(x,1e-10),tt_matrix(y,1e-10))-tt_matrix(rhs1))
+    %keyboard;
+    %norm(tt_matrix(mat)*kron(tt_matrix(x,1e-10),tt_matrix(y,1e-10))-tt_mat
+    %rix(rhs1))
+end
+return
