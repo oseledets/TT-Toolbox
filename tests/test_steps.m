d6b52c2c9c2dfa8887195a0625ea2246f79a42c6
dc <dc@1.com>
2011-05-09 15:21:38 +0200

    Some updates of test_steps

--- tests/test_steps.m
+++ tests/test_steps.m
@@ -1,13 +1,14 @@
 d0t = 7; % quantics dims for t
-d0x = 10; % quantics dims for x
+d0x = 8; % quantics dims for x
 dpx = 3; % phys. dims for x
 
 a = -10;
 b = 10;
+% a=0; b=1;
 h = (b-a)/(2^d0x+1);
 
 tol = 1e-6;
-eps = 1e-9;
+eps = 1e-8;
 maxit = 20;
 
 T = 10;
@@ -74,8 +75,11 @@ Ax = round(Ax, eps);
 
 norm_Ax = norm(Ax*x_ex)/norm(x_ex)
 
-u0 = x_ex;
+u0 = x_ex.*x_ex;
 
+
+% f1 = sin((a+(1:1:2^d0x)*h)'*pi);
+% f1 = 
 
 % Now, generate parts of KN scheme:
 KNm = tt_matrix(tt_eye(2,dpx*d0x))/tau - Ax*0.5;
@@ -84,12 +88,13 @@ KNm = round(KNm, eps);
 KNp = tt_matrix(tt_eye(2,dpx*d0x))/tau + Ax*0.5;
 KNp = round(KNp, eps);
 
+results = zeros(2^d0t,1);
 u = u0;
 for t=1:1:2^d0t
     u_new = mvk(KNm, u, tol, 20, u, 1000); % 1\tI - A/2
     
-    norm_rhs = mvk(KNp',u_new,tol,20,u_new,1000);
-    u = u_new;
+%     norm_rhs = mvk(KNp',u_new,tol,20,u_new,1000);
+%     u = u_new;
     for i=1:maxit
         tic;
         u = dmrg_solve2(KNp, u_new, u, tol, [], [], 1, KNp');
@@ -97,16 +102,17 @@ for t=1:1:2^d0t
     
         Mx = mvk(KNp,u,tol,20,u,1000);
         resid_true = norm(Mx-u_new)/norm(u_new);
-        MMx = mvk(KNp',Mx,tol,20,u,1000);
-        resid = norm(MMx - norm_rhs)/norm(norm_rhs);
+%         MMx = mvk(KNp',Mx,tol,20,u,1000);
+%         resid = norm(MMx - norm_rhs)/norm(norm_rhs);
         
-        fprintf('\n\n\t cur_time: %g\n\t true_resid: %3.3e\n\t norm.resid: %3.3e\n\t erank: %g\n', cur_time, resid_true, resid, erank(u));
+        fprintf('\n\n\t cur_time: %g\n\t true_resid: %3.3e\n\t erank: %g\n', cur_time, resid_true, erank(u));
         pause(0.5);
         if (resid_true<5*tol) 
             break; 
         end;
     end;
     
-    fprintf('\nTime step %d (%3.5e) done. Dist. to u0: %3.3e\n', t, t*tau, norm(u-u0)/norm(u0));
+    fprintf('\nTime step %d (%3.5e) done. Au/u: %3.3e\n', t, t*tau, norm(Ax*u)/norm(u));
+    results(t) = norm(Ax*u)/norm(u);
     pause(1);
 end;
