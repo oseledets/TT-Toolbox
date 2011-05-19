bb1ad3fe2f81a1e726c92ee3994d917b2bbdc2aa
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-05-19 12:58:48 +0200

    Simple test (wround_test.m) that shows that wround.m works completely incorrect: Just spoil the one part of the vector, set correct weights and get an incorrect result

---
+++ tests/wround_test.m
@@ -0,0 +1,14 @@
+d=10; n=2^d;
+a=0; b=1; h=(b-a)/(n-1);
+x=a+(0:n-1)*h; true_x=tt_tensor(reshape(x,2*ones(1,d)),1e-8);
+x_p=x;
+x_p(1:n/2)=randn(n/2,1);
+xx=tt_tensor(reshape(x_p,2*ones(1,d)),1e-12); 
+%Generate weight
+w=ones(1,n);
+w(1:n/2)=0;
+ww=tt_tensor(reshape(w,2*ones(1,d)),1e-12);
+%wround(x,ww,
+norm(diag(ww)*(xx-true_x))
+xxx=wround(xx,ww,1e-8);
+plot(full(xxx,[n,1]))
