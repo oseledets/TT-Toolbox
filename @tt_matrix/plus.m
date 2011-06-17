2389840de7c71eea2949d1c92952e0cc75319723
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-06-17 08:26:18 -0700

    Minor

--- @tt_matrix/plus.m
+++ @tt_matrix/plus.m
@@ -1,7 +1,14 @@
 function [a]=plus(b,c)
 %[A]=PLUS(B,C)
 %Adds two TT-matrices B and C
-a=tt_matrix; 
+if ( isempty(b) )
+   a=c;
+   return
+elseif (isempty(c) )
+   a=b;
+   return
+end
+    a=tt_matrix;
 a.n=b.n;
 a.m=b.m;
 a.tt=b.tt+c.tt;
