3db877fe0eca87fb4d54ac7883560305b2e066d9
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-07-04 14:24:05 +0200

    New function tt_rand: Functionality more or less identical to tt_random, Result is a tt_tensor, and r(1) and r(d+1) can be not equal to 1

---
+++ core/tt_rand.m
@@ -0,0 +1,26 @@
+function [tt]=tt_rand(n,d,r)
+%[TT]=TT_RAND(N,D,R)
+%Generate a random tensor
+%N --- is a mode size array
+%It can be either a number, or an array of dimension d
+%R --- rank array.
+%Either a number, or an array of dimension d+1
+
+if ( numel(n) == 1 ) 
+ n = n * ones(1,d);
+end
+if ( numel(r) == 1 )
+ r = r * ones(1,d+1); 
+ r(1) = 1;
+ r(d+1)=1;
+end
+tt=tt_tensor;
+tt.n=n;
+tt.r=r;
+tt.d=d;
+ps=cumsum([1;n.*r(1:d).*r(2:d+1)]);
+tt.ps=ps;
+cr=randn(ps(d+1)-1,1);
+tt.core=cr;
+return
+end
