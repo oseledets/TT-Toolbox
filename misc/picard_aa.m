463f3b9e564e69a1f6d776e45d2091edc09cac13
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-07-26 14:35:02 +0200

    Added a process_fun option to picard_aa, if you want to draw something in process, use it

--- misc/picard_aa.m
+++ misc/picard_aa.m
@@ -7,15 +7,17 @@ function [sol]=picard_aa(x0,f,eps,varargin)
 %eps -- tolerance
 %Optional parameters (supplied as keys, i.e. 'niters', 100)
 %niters --- maximal number of iterations, default: 100
-%m --- dimension of the AA space, default: 100
+%restart --- dimension of the AA space, default: 100
 %warmup --- number of fixed point iterations before AA starts, default: 1
 %verb --- print convergence information, default: true
 %fixed_iter --- if set to true, only the fixed point iteration is run, default  --- false 
-
+%process_fun --- if found, apply this function to compute some information about the solution
+%(i.e. draw it, or compute some functional from it, etc)
 warmup=1;
 niters=100;
 m=100;
 verb=true;
+draw=false;
 fixed_iter=false;
 for i=1:2:length(varargin)-1
     switch lower(varargin{i})
@@ -29,6 +31,9 @@ for i=1:2:length(varargin)-1
             verb=varargin{i+1};
         case 'fixed_iter'
             fixed_iter=varargin{i+1};
+        case 'process_fun'
+           draw=true;
+           process_fun=varargin{i+1}; 
         otherwise
             error('Unrecognized option: %s\n',varargin{i});
     end
@@ -80,24 +85,13 @@ while ( k < niters && er > eps )
     dx=xc*gd';
     %New x
     xnew=rhs + xc(:,end)-(dx+Fk)*gm;
-    m1=sqrt(numel(xnew));
-    mesh(reshape(xnew,m1,m1));
-    drawnow
-    %Test sectant identity
- 
-    %norm(Gk*Fk-dx)
+    
+    if ( draw ) 
+       process_fun(xnew);
+    end
     
     xc=[xc,xnew];
     res=[res,f(xnew)-xnew];
-    %m0=2; [-1,1]
-    %[a,b]*[-1]->b-a
-    %      [1]
-    
-   
-    %
-    %Take differences of res;
-    %this needs a simple discrete gradient matrix
-    %[xc(1),xc(2)-xc(1),...,
     k=k+1;
 end
 sol=xc(end);
