4500f96aad2624ea10f6973c2b025206f07cd6d3
Vladimir Kazeev <kva@kva-laptop.(none)>
2011-08-11 15:30:07 +0200

    four tiny functions for the QTT format

---
+++ core/tt_qtofull.m
@@ -0,0 +1,36 @@
+function [full]=tt_qtofull(tt,s)
+
+% Returns a tensor _full_, given in a QTT decomposition _tt_,
+% k-th core of which is indexed by
+% i^{k}_{1} ,..., i^{k}_{s},
+% k=1,...,d,
+% in the full-format representation.
+% The output tensor _full_ is indexed by
+% i^{1}_{1} ,..., i^{d}_{1} ,...,..., i^{1}_{s} ,..., i^{d}_{s},
+%
+% April 26, 2011
+% Vladimir Kazeev
+% vladimir.kazeev@gmail.com
+% INM RAS
+% Moscow, Russia
+%
+
+d=size(tt,1);
+sz=tt_qsize(tt,s);
+
+tt=tt_qreshape(tt,s,prod(sz,2));
+full=tt_to_full(tt);
+full=reshape(full,reshape(sz',1,numel(sz)));
+
+prm=zeros(1,s*d);
+for k=1:d
+	for l=1:s
+		prm(s*k+1-l)=k+(s-l)*d;
+	end
+end
+
+full=ipermute(full,prm);
+full=reshape(full,[prod(sz,1),1]);
+
+return
+end
