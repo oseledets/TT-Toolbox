f58bc0230468ecfc1600da6483c36257e3bde4f1
Vladimir Kazeev <kva@kva-laptop.(none)>
2011-08-12 11:01:05 +0200

    The function reverses a TT train

---
+++ core/tt_reverse.m
@@ -0,0 +1,25 @@
+function [tt2]=tt_reverse(tt,phd)
+
+% Returns a reversed TT rperesentation
+%
+% January 25, 2011
+% Vladimir Kazeev
+% vladimir.kazeev@gmail.com
+% INM RAS
+% Moscow, Russia
+%
+
+d=size(tt,1);
+tt2=cell(d,1);
+
+tt2{1}=tt{d};
+for k=2:d-1
+	prm=(1:phd+2);
+	prm(phd+1)=phd+2;
+	prm(phd+2)=phd+1;
+	tt2{k}=permute(tt{d+1-k},prm);
+end
+tt2{d}=tt{1};
+
+return
+end
