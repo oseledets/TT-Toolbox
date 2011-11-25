86436a3c03e3fe134d5d95ab7e896d17decb7947
Ivan Oseledets <ivan.oseledets@gmail.com>
2011-11-25 14:35:20 +0400

    Round seems to work in the simplest case

--- @qtt_tucker/round.m
+++ @qtt_tucker/round.m
@@ -11,8 +11,23 @@ for i=1:d
 end
 core=round(core,eps); %Round the core --- we know the result comes
 %with lr orthogonality?
-for i=d:-1:2
-   %Take the last core then 
+rtt=rank(core); 
+n=size(core);
+for i=1:d
+   cr=reshape(core{i},[rtt(i),n(i),rtt(i+1)]);
+   cr=permute(cr,[2,1,3]); cr=reshape(cr,n(i),rtt(i)*rtt(i+1));
+   [u,s,v]=svd(cr,'econ');
+   s=diag(s);
+   r=my_chop2(s,norm(s)*eps);
+   u=u(:,1:r); s=s(1:r); v=v(:,1:r);
+   tuck{i}=tuck{i}*(u*diag(s)); 
+   tuck{i}=round(tuck{i},eps);
+   [tuck{i},rm]=qr(tuck{i},'lr');
+   cr=rm*v';
+   cr=reshape(cr,[r,rtt(i),rtt(i+1)]);
+   core{i}=permute(cr,[2,1,3]);
+   %The result is lr-orthogonal, thus we need only to 
 end
+   
 return
 end
