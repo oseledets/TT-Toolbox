88b793fe3fe12394be091a53dc996b0f2dd8908f
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-01-25 17:52:32 +0100

    Incorporated O.S. Lebedeva fix to the col function

--- @tt_tensor/col.m
+++ @tt_tensor/col.m
@@ -1,40 +1,25 @@
-function [y]=col(x,k)
+function [y]=col(x, k, varargin)
 %[Y]=COL(X,K)
 %Computes the subcolumns of a 
 %It is useful for block algorithms when r(d+1) is not equal to 1
 %For analogous thing on r(1) see ROW(X,K)
-
-%d=x.d;
-%r=x.r;
-%n=x.n;
-%cr=x.core;
-%ps=x.ps;
-%y=tt_tensor;
-
 d=x.d;
-rx=x.r;
-nx=x.n;
-cr=subsref(x,substruct('{}',{d}));
-cr=reshape(cr,[numel(cr)/rx(d+1),rx(d+1)]);
-cr=cr(:,k);
-cr=reshape(cr,[rx(d),nx(d),numel(k)]);
-%x{d}=reshape(x{d},[rx(d),nx(d),numel(k)]);
-x=subsasgn(x,substruct('{}',{d}),cr);
-y=x;
+r=x.r;
 
-%Modification is required only for the last core
-%cr_last=cr(ps(d):ps(d+1)-1);
-%cr_last=reshape(cr_last,[r(d),n(d),r(d+1)]);
-%cr_last=cr_last(:,:,k);
-%r(d+1)=size(cr_last,3);
-%cr(ps(d):ps(d+1)-1)=[];
-%cr=[cr,cr_last(:)'];
-%r(d+1)=size(cr_last,3);
-%ps(d+1)=ps(d)+r(d)*n(d)*r(d+1);
-%y.n=n;
-%y.d=d;
-%y.r=r;
-%y.ps=ps;
-%y.core=cr;
-%return
+if nargin > 2
+    for j = 1 : nargin-2
+        k = [k, varargin{j}];
+    end
+end
+
+BlockSize = r(d+1);
+if max(k) > BlockSize
+    error('TT-tensor.col:Index exceeds dimensions in TT block.');
+end
+
+CutMatr = eye(BlockSize);
+CutMatr = CutMatr(:, k);
+y = x*CutMatr;  
+
+return
 end
