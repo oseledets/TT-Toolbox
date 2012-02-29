c1f71b837f9ada2f5ea436dd3bd1cda5f0cca052
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-02-29 16:25:01 +0400

    Fixed a nasty bug in the "reort" procedure. It seems, that this bug can harm a lot of "advanced" subroutines

--- core/reort.m
+++ core/reort.m
@@ -25,24 +25,21 @@ end
 if (size(u,2) + size(uadd,2) >= size(u,1) )
   uadd=uadd(:,size(u,1)-size(u,2));
 end
-radd=size(uadd,2);
 
 mvr=u'*uadd; unew=uadd-u*mvr; 
 reort_flag=true;
 while (reort_flag )
-    reort_flag=false;
-    j=1;
-%Nice vectorization!
-norm_unew=sum(unew.^2,1); 
-norm_uadd=sum(uadd.^2,1);
-reort_flag=isempty(norm_unew <= 0.25*norm_uadd);
-[unew,rv_not_used]=qr(unew,0); %Here it is ok.
-if (reort_flag)
-  su=u'*unew;
-  uadd=unew;
-  unew=unew-u*su; 
-end
-  
+    %Nice vectorization!
+    norm_unew=sum(unew.^2,1);
+    norm_uadd=sum(uadd.^2,1);
+    reort_flag=~isempty(find(norm_unew <= 0.25*norm_uadd,1));
+    [unew,~]=qr(unew,0); %Here it is ok.
+    if (reort_flag)
+        su=u'*unew;
+        uadd=unew;
+        unew=unew-u*su;
+    end
+    
 end
 y=[u,unew];
 return
