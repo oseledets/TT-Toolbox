3d14111f97e13997c2d8ffcbcb2f08b2bb9d36b8
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-03-01 18:05:24 +0300

    Maybe randomizer will help?

--- core/reort.m
+++ core/reort.m
@@ -29,7 +29,8 @@ end
 mvr=u'*uadd; unew=uadd-u*mvr; 
 reort_flag=true;
 j=1;
-while (reort_flag && j <= 2)
+z=[];
+while (reort_flag && j <= 20 )
     %Nice vectorization!
     norm_unew=sum(unew.^2,1);
     norm_uadd=sum(uadd.^2,1);
@@ -43,15 +44,23 @@ while (reort_flag && j <= 2)
         su=u'*unew;
         unew=unew-u*su;
         %Kill machine zeros
-        unew(unew==0)=max(1e-308,norm(unew,'fro')*1e-15);
+        %unew(unew==0)=max(1e-308,norm(unew,'fro')*1e-15);
         j=j+1;
+        if ( j == 3 && isempty(z) ) %Kill machine zeros
+           z=randn(size(unew,1),1); z=z/norm(z);
+           u = u - 2*z*(z'*u);
+           unew = unew - 2*z*(z'*unew);
+        end
     end
 end
 if ( reort_flag )
  fprintf('Reort failed to reort! \n');
- [y,~]=qr([u,unew]);
+ [y,~]=qr([u,unew],0);
 else
  y=[u,unew];
-%end
+end
+if ( ~isempty(z) ) 
+   y = y - 2*z*(z'*y);
+end
 return
 end
