af4e47f0d45110678ee5e8794c56a1092ae6e6b5
dc <sergey.v.dolgov@gmail.com>
2012-08-21 15:22:30 +0200

    fixes in tt_ind2sub. Now vectorized

--- core/tt_ind2sub.m
+++ core/tt_ind2sub.m
@@ -15,20 +15,21 @@ function [ind] = tt_ind2sub(siz,ndx)
 %ind=tt_ind2sub(size,ndx)
 
 %nout = max(nargout,1);
-nout = size(siz,2);
+% nout = size(siz,2);
+n = numel(siz);
 
-siz = double(siz);
-ind=zeros(1,nout);
-if length(siz)<=nout,
-  siz = [siz ones(1,nout-length(siz))];
-else
-  siz = [siz(1:nout-1) prod(siz(nout:end))];
-end
-n = length(siz);
+% siz = double(siz);
+ind=zeros(size(ndx,1),n);
+% if length(siz)<=nout,
+%   siz = [siz ones(1,nout-length(siz))];
+% else
+%   siz = [siz(1:nout-1) prod(siz(nout:end))];
+% end
+% n = length(siz);
 k = [1 cumprod(siz(1:end-1))];
 for i = n:-1:1,
   vi = rem(ndx-1, k(i)) + 1;         
   vj = (ndx - vi)/k(i) + 1; 
-  ind(i) = vj; 
+  ind(:,i) = vj; 
   ndx = vi;     
 end
