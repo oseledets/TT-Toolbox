d646c6ab8019f324858cc93d113ec9f6cfb14ceb
Dmitry Savostyanov <drraug@gmail.com>
2013-01-08 11:00:03 +0000

    Fix typo & error: matrices are n-by-m, not blas-like.

--- @tt_matrix/kron1.m
+++ @tt_matrix/kron1.m
@@ -24,8 +24,8 @@ if isempty(b)
 end
 if ndims(a.tt) == ndims(b.tt)
     c=tt_matrix;
-    ma = a.m; mb = b.m;
     na = a.n; nb = b.n;
+    ma = a.m; mb = b.m;
     c.n=na.*nb;
     c.m=ma.*mb; 
     d = ndims(a.tt);
@@ -33,13 +33,13 @@ if ndims(a.tt) == ndims(b.tt)
     c.tt.d = d;
     c.tt.r = ra.*rb;
     c.tt.n = c.m.*c.n;
-    c.tt.ps = cumsum([1; c.tt.r(1:d).*c.tt.n*c.tt.r(2:d+1)]);
+    c.tt.ps = cumsum([1; c.tt.r(1:d).*c.tt.n.*c.tt.r(2:d+1)]);
     c.tt.core = zeros(c.tt.ps(d+1)-1, 1);
     for i=1:d
-        cra = reshape(a.tt{i}, [ra(i)*ma(i),na(i)*ra(i+1)]);
-        crb = reshape(b.tt{i}, [rb(i)*mb(i),nb(i)*rb(i+1)]);
+        cra = reshape(a.tt{i}, [ra(i)*na(i),ma(i)*ra(i+1)]);
+        crb = reshape(b.tt{i}, [rb(i)*nb(i),mb(i)*rb(i+1)]);
         crc = kron(crb,cra);
-        crc = reshape(crc, [ra(i),ma(i),rb(i),mb(i),na(i),ra(i+1),nb(i),rb(i+1)]);
+        crc = reshape(crc, [ra(i),na(i),rb(i),nb(i),ma(i),ra(i+1),mb(i),rb(i+1)]);
         crc = permute(crc, [1, 3, 4, 2, 7, 5, 6, 8]);
         c.tt.core(c.tt.ps(i):c.tt.ps(i+1)-1) = crc(:);
     end    
