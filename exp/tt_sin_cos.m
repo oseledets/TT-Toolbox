90084a8d93054cc424c0c0f6711ec8ad60df97a3
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-03-21 18:28:08 +0400

    tt_sin_cos fix

--- exp/tt_sin_cos.m
+++ exp/tt_sin_cos.m
@@ -8,13 +8,13 @@
 function [tts] = tt_sin_cos(d, alpha, type)
 
 tts = cell(d,1);
-tts{1} = zeros(2,2);
+tts{1} = zeros(1, 2, 2);
 if (type==0)
-    tts{1}(1:2,1) = [1 cos(alpha)]';
-    tts{1}(1:2,2) = [0 sin(alpha)]';
+    tts{1}(1, 1:2, 1) = [1 cos(alpha)]';
+    tts{1}(1, 1:2, 2) = [0 sin(alpha)]';
 else
-    tts{1}(1:2,1) = [0 -sin(alpha)]';
-    tts{1}(1:2,2) = [1 cos(alpha)]';    
+    tts{1}(1, 1:2, 1) = [0 -sin(alpha)]';
+    tts{1}(1, 1:2, 2) = [1 cos(alpha)]';    
 end;
 
 for q=2:d-1
@@ -33,6 +33,13 @@ tts{d}(1:2,1)=[0 1]';
 % core col for n_d = 1:
 tts{d}(1:2,2)=[sin((2^(d-1))*alpha) cos((2^(d-1))*alpha)]';
 
-tts = tt_tensor(tts);
+tt1 = tt_tensor();
+%tt1.d = d;
+%tt1.ps = tt1.get_ps();
+%tt1.n = 2 * ones(1, d);
+%tt1.r = [1; 2 * ones(1, d - 1); 1];
+tt1 = cell2core(tt1, tts);
+tts = tt1;
+%tts = tt_tensor(tts);
 
 end
