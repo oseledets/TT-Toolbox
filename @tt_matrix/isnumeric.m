5e244fb655b414d93250ba6b878f7b0000cecfb5
dc <sergey.v.dolgov@gmail.com>
2013-07-01 12:09:05 +0100

    isnumeric is overloaded to return 1 on tt_matrix and tt_tensor

---
+++ @tt_matrix/isnumeric.m
@@ -0,0 +1,20 @@
+function [o]=isnumeric(tt)
+%O = ISNUMERIC(TT)
+%   Returns the logical 1 if the input is a tt_matrix.
+%
+%
+% TT-Toolbox 2.2, 2009-2012
+%
+%This is TT Toolbox, written by Ivan Oseledets et al.
+%Institute of Numerical Mathematics, Moscow, Russia
+%webpage: http://spring.inm.ras.ru/osel
+%
+%For all questions, bugs and suggestions please mail
+%ivan.oseledets@gmail.com
+%---------------------------
+
+o = false;
+if (isa(tt, 'tt_matrix'))
+    o = true;
+end;
+end
