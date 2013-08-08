7de65efc1179b2938a6bad5e05d3f59797a4a4a6
Dmitry Savostyanov <drraug@gmail.com>
2013-08-08 15:03:11 +0100

    nnz added for matrices and tensors

---
+++ @tt_matrix/nnz.m
@@ -0,0 +1,17 @@
+function [n]=nnz(a)
+% Returns TT-ranks of the given tensor in TT-format
+%   [N]=NNZ(A) Returns number of non zero elements in the TT representation of A
+%
+% TT-Toolbox 2.2, 2009-2012
+%
+%This is TT Toolbox, written by Ivan Oseledets et al.
+%Institute of Numerical Mathematics, Moscow, Russia
+%webpage: http://spring.inm.ras.ru/osel
+%
+%For all questions, bugs and suggestions please mail
+%ivan.oseledets@gmail.com
+%---------------------------
+
+n=nnz(a.tt.core)
+return
+end
