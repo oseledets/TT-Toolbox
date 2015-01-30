19a49c4bf543a92c5cfa2de65f9e49cc13ac823e
Sergey Dolgov <dolgov@mpi-magdeburg.mpg.de>
2015-01-30 15:29:24 +0100

    tt_unit accepts multiindices

--- core/tt_unit.m
+++ core/tt_unit.m
@@ -21,13 +21,13 @@ function [tt] = tt_unit(n,varargin)
 
 if (numel(n) == 1)
     if (numel(varargin)>0)
-        d=varargin{1}; 
+        d=varargin{1};
     else
         d=1;
     end;
-  n=n*ones(1,d);
+    n=n*ones(1,d);
 else
-  d=numel(n);
+    d=numel(n);
 end
 if (nargin>2)
     j = varargin{2};
@@ -37,18 +37,15 @@ end;
 
 tt=cell(d,1);
 
-if j==1
-    for k=1:d
-        tt{k} = zeros(n(k),1);
-        tt{k}(1) = 1;
-    end
-else
-    jj = tt_ind2sub(reshape(n, 1, d), j);
-    for k=1:d
-        tt{k} = zeros(n(k),1);
-        tt{k}(jj(k)) = 1;
-    end
+if (numel(j)==1)
+    j = tt_ind2sub(reshape(n, 1,[]), j);
+end;
+
+for k=1:d
+    tt{k} = zeros(n(k),1);
+    tt{k}(j(k)) = 1;
 end
+
 tt=tt_tensor(tt); %Bydlocode @
-return
+
 end
