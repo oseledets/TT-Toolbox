400c17433beccf6f4fa6950049019cae4cdf19d0
Alexander Novikov <novikov@bayesgroup.ru>
2015-05-08 17:39:28 +0300

    Add guards to outdated functions

--- @tt_tensor/move2gpu.m
+++ @tt_tensor/move2gpu.m
@@ -2,6 +2,7 @@ function x = move2gpu(x, toSingle)
     % Move TT-tensor to the GPU.
     % toSingle is boolean variable. If true, store everything on the GPU as
     % single.
+    error('I''m pretty sure you don''t need this function. cnn_train calls vl_simplenn_move, which moves all layers (including the TT ones) to GPU when appropriate.')
     if nargin <= 1
         toSingle = false;
     end
