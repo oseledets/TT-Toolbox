c0c4e948acc98134e5ea0743b21bb9d85ecbed4b
Alexey Boyko <boyko.phystech@gmail.com>
2016-03-18 14:25:51 +0300

    mtkron now accepts a cell array of tt_tensors as a single input

--- core/mtkron.m
+++ core/mtkron.m
@@ -1,20 +1,30 @@
 function z=mtkron(varargin)
-%"Tensor" Kronecker product (in "TT" order) of multiple matrices
-%   Z=MTKRON(A,B,C,...) Takes Kronecker product of the arguments
-%   See also TT_TENSOR/TKRON, TT_MATRIX/TKRON 
-%
-% TT-Toolbox 2.2, 2009-2012
-%
-%This is TT Toolbox, written by Ivan Oseledets et al.
-%Institute of Numerical Mathematics, Moscow, Russia
-%webpage: http://spring.inm.ras.ru/osel
-%
-%For all questions, bugs and suggestions please mail
-%ivan.oseledets@gmail.com
-%---------------------------
+    %"Tensor" Kronecker product (in "TT" order) of multiple matrices
+    %   Z=MTKRON(A,B,C,...) Takes Kronecker product of the arguments
+    %   See also TT_TENSOR/TKRON, TT_MATRIX/TKRON 
+    %
+    %   
+    %   it also takes a cell array of tt_tensors as a single input
+    %
+    % TT-Toolbox 2.2.1, 2009-2016
+    %
+    %This is TT Toolbox, written by Ivan Oseledets et al.
+    %Institute of Numerical Mathematics, Moscow, Russia
+    %webpage: http://spring.inm.ras.ru/osel
+    %
+    %For all questions, bugs and suggestions please mail
+    %ivan.oseledets@gmail.com
+    %---------------------------
+    %Last tweaks by Alexey Boyko of Oseledets' group
+    %alexey.boyko@skolkovotech.ru
 
-z=varargin{1};
-for i=2:numel(varargin)
-  z=tkron(z,varargin{i});
-end
+    z=varargin{1};
+    if numel(z)~=1
+        % in case it a cell array of tt_tensors as a single input
+        varargin=z;
+        z=varargin{1};
+    end
+    for i=2:numel(varargin)
+      z=tkron(z,varargin{i});
+    end
 end
